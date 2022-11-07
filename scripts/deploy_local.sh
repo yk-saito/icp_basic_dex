9d479061c40f6b4c5aa34de7a179a18a27868fb5
Yuka Saito <yksaito02@gmail.com>
2022-11-08 02:47:18 +0900

    mod: delete deploy option

--- scripts/deploy_local.sh
+++ scripts/deploy_local.sh
@@ -13,12 +13,12 @@ echo $ROOT_PRINCIPAL
 dfx start --clean --background
 
 # register, build, and deploy a dapp
-dfx deploy HogeDIP20 --network local --argument='("Token Hoge Logo", "Token Hoge", "THG", 8, 10000000000000000, principal '\"$ROOT_PRINCIPAL\"', 0)'
-dfx deploy PiyoDIP20 --network local --argument='("Token Piyo Logo", "Token Piyo", "TPY", 8, 10000000000000000, principal '\"$ROOT_PRINCIPAL\"', 0)'
+dfx deploy HogeDIP20 --argument='("Token Hoge Logo", "Token Hoge", "THG", 8, 10000000000000000, principal '\"$ROOT_PRINCIPAL\"', 0)'
+dfx deploy PiyoDIP20 --argument='("Token Piyo Logo", "Token Piyo", "TPY", 8, 10000000000000000, principal '\"$ROOT_PRINCIPAL\"', 0)'
 
 # deploy Internet Identity
-dfx deploy internet_identity --network local
+dfx deploy internet_identity
 
-dfx deploy icp_basic_dex_backend --network local
+dfx deploy icp_basic_dex_backend
 
-dfx deploy  icp_basic_dex_frontend --network local
+dfx deploy  icp_basic_dex_frontend
